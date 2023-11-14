222bc3627678c5bb67200cd4a6112c373fbff0d3
lcaffarone <lcaffarone@udesa.edu.ar>
2023-11-13 22:37:56 -0300

    genres

--- js/generos.js
+++ js/generos.js
@@ -24,3 +24,22 @@ fetch(urlGenerosPelis)
 .catch(function(miError){
     console.log(miError);
 }); 
+fetch(urlGenerosseries)
+.then(function(miRespuesta){
+    return miRespuesta.json();
+}
+)
+.then(function(miRespuesta){
+    let generos= miRespuesta.genres;
+    console.log(generos);
+
+    let contenido = "";
+    for (let  i = 0; i <12; i++){
+        contenido += `<li><a href ="./detallegenero.html?id_genero=${generos[i].id}&nombre=${generos[i].name}">${generos[i].name} `
+    }
+
+    listaSeries.innerHTML = contenido;
+})
+.catch(function(miError){
+    console.log(miError);
+});
